7a8f188d4455f7dd9e62a030344583bfec060006
OzgeOzyurt <ozgeozyurt00@gmail.com>
2023-11-07 02:26:51 +0300

    Geliştirme: Admin Kullanıcı Yönetim İşlevselliği ve DataAnnotations Optimizasyonları

--- OtoServisSatis.Entities/Rol.cs
+++ OtoServisSatis.Entities/Rol.cs
@@ -1,8 +1,13 @@
-﻿namespace OtoServisSatis.Entities
-{//5
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace OtoServisSatis.Entities
+{
     public class Rol : IEntity
     {
         public int Id { get; set; }
+
+        [StringLength(50),  Display(Name = "Rol Adı"), Required(ErrorMessage = "{0} boş bırakılamaz!")]
         public string Adi { get; set; }
     }
 }
