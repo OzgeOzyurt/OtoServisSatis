57c1dcbc0e600850ffd70f4432faaa9cdd58a059
OzgeOzyurt <ozgeozyurt00@gmail.com>
2023-11-09 13:22:29 +0300

    Araç ve Slider Modülüne Resim Yükleme Özelliklerinin Eklenmesi

--- OtoServisSatis.Entities/Arac.cs
+++ OtoServisSatis.Entities/Arac.cs
@@ -30,7 +30,16 @@ namespace OtoServisSatis.Entities
         
         [Required(ErrorMessage = "{0} boş bırakılamaz!")]
         public string Notlar { get; set; }
-       
+        
+        [StringLength(100)]
+        public string? Resim1 { get; set; }
+        
+        [StringLength(100)]
+        public string? Resim2 { get; set; }
+        
+        [StringLength(100)]
+        public string? Resim3 { get; set; }
+
         public virtual Marka? Marka { get; set; } //Araç sınıfı ile Marka sınıf arasında bağlantı
     }
 }
