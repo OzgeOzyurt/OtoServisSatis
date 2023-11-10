2dd6e88b76b9bb3439b0438c306488a6e0f62ce3
OzgeOzyurt <ozgeozyurt00@gmail.com>
2023-11-10 19:09:23 +0300

    Kullanıcı Yönetimi ve Güvenlik Güncellemeleri

---
+++ OtoServisSatis.Data/Migrations/20231110131410_UserGuidEklendi..cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OtoServisSatis.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserGuidEklendi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserGuid",
+                table: "Kullanicilar",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Kullanicilar",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "EklenmeTarihi", "UserGuid" },
+                values: new object[] { new DateTime(2023, 11, 10, 16, 14, 9, 976, DateTimeKind.Local).AddTicks(50), new Guid("275e367e-daf6-42b0-a8fc-05f20eca848b") });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserGuid",
+                table: "Kullanicilar");
+
+            migrationBuilder.UpdateData(
+                table: "Kullanicilar",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "EklenmeTarihi",
+                value: new DateTime(2023, 11, 9, 14, 13, 41, 751, DateTimeKind.Local).AddTicks(347));
+        }
+    }
+}
---
+++ OtoServisSatis.Service/Concrete/UserService.cs
@@ -0,0 +1,13 @@
+﻿using OtoServisSatis.Data;
+using OtoServisSatis.Service.Abstract;
+using OtoServisSatis.Data.Concrete;
+
+namespace OtoServisSatis.Service.Concrete
+{
+    public class UserService : UserRepository, IUserService
+    {
+        public UserService(DatabaseContext context) : base(context)
+        {
+        }
+    }
+}
